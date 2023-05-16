5826b068e7d39c76a729b685f12ebd31da64e44f
was1002 <wochna.andras@gmail.com>
2023-05-16 16:03:23 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,84 @@
+import argparse
+import cv2
+from ultralytics import YOLO
+import supervision as sv
+import numpy as np
+
+
+ZONE_POLYGON = np.array([
+    [0.2, 0.2],
+    [0.8, 0.2],
+    [0.8, 0.8],
+    [0.2, 0.8]
+])
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="YOLOv8 live")
+    parser.add_argument(
+        "--webcam-resolution",
+        default=[2560, 720],  # [1344, 376],  #
+        nargs=2,
+        type=int
+    )
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_arguments()
+    frame_width, frame_height = args.webcam_resolution
+    cap = cv2.VideoCapture(1)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+    left_frame_width = int(frame_width/2)
+    resolution = tuple((left_frame_width, frame_height))
+    model = YOLO("yolov8l.pt")
+
+    box_annotator = sv.BoxAnnotator(
+        thickness=2,
+        text_thickness=2,
+        text_scale=1
+    )
+
+    zone_polygon = (ZONE_POLYGON * np.array(resolution)).astype(int)
+    zone = sv.PolygonZone(polygon=zone_polygon, frame_resolution_wh=resolution)
+    zone_annotator = sv.PolygonZoneAnnotator(
+        zone=zone,
+        color=sv.Color.red(),
+        thickness=2,
+        text_thickness=4,
+        text_scale=2
+    )
+
+    while True:
+        ret, frame = cap.read()
+        left_frame = frame[0:resolution[1], 0:resolution[0]].copy()
+
+        result = model(left_frame, agnostic_nms=True)[0]
+        detections = sv.Detections.from_yolov8(result)
+        detections = detections[detections.class_id == 0]
+        labels = [
+            f"{model.model.names[class_id]} {confidence:0.2f}"
+            for _, confidence, class_id, _
+            in detections
+        ]
+        left_frame = box_annotator.annotate(
+            scene=left_frame,
+            detections=detections,
+            labels=labels
+        )
+
+        zone.trigger(detections=detections)
+        left_frame = zone_annotator.annotate(scene=left_frame)
+
+        cv2.imshow("yolov8", left_frame)
+
+        print(left_frame.shape)
+        print(frame.shape)
+        # break
+        if cv2.waitKey(30) == 27:
+            break
+
+
+if __name__ == "__main__":
+    main()
